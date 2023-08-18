930a99740369b40a7ec9c18a65bd2bee87c3263c
sWD4 <shubham2405@gmail.com>
2023-08-18 17:31:19 +0530

    changes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,113 +11,114 @@ cloudinary.config({
 });
 
 class UserController {
-
     static userinsert = async (req, res) => {
-
-        //console.log(req.files.image)
-        const file = req.files.image
+        // console.log(req.files.image)
+        const file = req.files.image;
         const imageUpload = await cloudinary.uploader.upload(file.tempFilePath, {
-
-            folder: 'studentimage'
-        })
+            folder: "studentimage",
+        });
         // console.log(imageUpload)
-        const { name, email, password, cpassword } = req.body
-        const user = await UserModel.findOne({ email: email })
+        const { name, email, password, cpassword } = req.body;
+        const user = await UserModal.findOne({ email: email });
         //console.log(user)
         if (user) {
-            res.status(401)
+            res
+                .status(401)
                 .json({ status: "failed", message: "ᴛʜɪꜱ ᴇᴍᴀɪʟ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴇxɪᴛꜱ😓" });
         } else {
             if (name && email && password && cpassword) {
                 if (password == cpassword) {
-
                     try {
-                        const hashpassword = await bcrypt.hash(password, 10)
-                        const result = new UserModel({
-
+                        const hashpassword = await bcrypt.hash(password, 10);
+                        const result = new UserModal({
                             name: name,
                             email: email,
                             password: hashpassword,
                             image: {
                                 public_id: imageUpload.public_id,
-                                url: imageUpload.secure_url
-                            }
-                        })
+                                url: imageUpload.secure_url,
+                            },
+                        });
 
-                        await result.save()
+                        await result.save();
 
-                        res.status(201)
-                            .json({ status: "Success", message: "Ragistration Successfully" });
-
+                        res.status(201).json({
+                            status: "Success",
+                            message: "Ragistration Successfully",
+                        });
                     } catch (error) {
-                        console.log(error)
+                        console.log(error);
                     }
-
                 } else {
-                    res.status(401)
-                        .json({ status: "failed", message: "Password and confirm password does not match" });
+                    res.status(401).json({
+                        status: "failed",
+                        message: "Password and confirm password does not match",
+                    });
                 }
             } else {
-                res.status(201)
+                res
+                    .status(201)
                     .json({ status: "failed", message: "All field are required" });
             }
         }
-
-    }
+    };
 
     static verify_login = async (req, res) => {
-
-
         try {
-
             //console.log(req.body)
 
-            const { email, password } = req.body
+            const { email, password } = req.body;
             if (email && password) {
-                const user = await UserModel.findOne({ email: email })
+                const user = await UserModal.findOne({ email: email });
                 // console.log(user)
                 if (user != null) {
-                    const ismatch = await bcrypt.compare(password, user.password)
+                    const ismatch = await bcrypt.compare(password, user.password);
                     if (ismatch) {
-                        const token = jwt.sign({ ID: user._id }, 'shivanibansal@123#8962');
-                        res.cookie('token', token)
+                        const token = jwt.sign({ ID: user._id }, "shivanibansal@123#8962");
+                        res.cookie("token", token);
                         res.status(200)
-                        .json({ status: "Success", message: "Login Successfully", token });
-                    }
-                    else {
+                            .json({ status: "Success", message: "Login Successfully", token, });
+                    } else {
                         res.status(401)
-                   .json({ status: "failed", message: "Email password is not valid" });
+                            .json({ status: "failed", message: "Email password is not valid", });
                     }
+                } else {
+                    res
+                        .status(401)
+                        .json({ status: "failed", message: "You are not register user" });
                 }
-                else {
-                    res.status(401)
-                .json({ status: "failed", message: "You are not register user" });
-                }
+            } else {
+                res
+                    .status(401)
+                    .json({ status: "failed", message: "All field are required" });
             }
-            else {
-                res.status(401)
-                .json({ status: "failed", message: "All field are required" });
-            }
-
-        } catch (error) {
-            console.log('error');
-        }
-
-    }
-
-    static logout = async (req, res) => {
-        try {
-            res.clearCookie("token");
-            res.send('logout successfully');
         } catch (error) {
             console.log("error");
         }
     };
 
-    static getuserdetail = async (req, res) => {
+    static logout = async (req, res) => {
         try {
-            //console.log(req.user);
-            const user = await userModel.findById(req.user.id);
+            res.clearCookie("token");
+            res.send("logout successfully");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static profile = async (req, res) => {
+        try {
+            const { name, email, id, image } = req.user;
+            res.render("profile", { n: name, e: email, image: image, message: req.flash("success"), error: req.flash("error"), });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static get_user_detail = async (req, res) => {
+        try {
+            // console.log(req.user);
+            const user = await UserModal.findById(req.user.id);
 
             res.status(200).json({
                 success: true,
@@ -128,18 +129,20 @@ class UserController {
         }
     }
 
-    static getalluser = async (req, res) => {
+    static get_all_user = async (req, res) => {
         try {
-            const getalluser = await UserModel.find();
+            // console.log(req.user);
+            const getalluser = await UserModal.find();
+
             res.status(200).json({
                 success: true,
                 getalluser,
-            })
+            });
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-     
+
     static change_password = async (req, res) => {
         try {
             const { name, email, id, image } = req.user
@@ -149,32 +152,32 @@ class UserController {
                 const user = await UserModel.findById(id)
                 const ismatch = await bcrypt.compare(oldpassword, user.password)
                 if (!ismatch) {
-                    
+
                     res.status(400)
-                .json({ status: "Failed", message: "Old Password is incorrect" });
+                        .json({ status: "Failed", message: "Old Password is incorrect" });
                 }
                 else {
                     if (newpassword !== cpassword) {
-                        
+
                         res.status(400)
-                        .json({ status: "Failed", message: "Password and confirm password is not matched" });
+                            .json({ status: "Failed", message: "Password and confirm password is not matched" });
                     }
                     else {
                         const newHashpassword = await bcrypt.hash(newpassword, 10)
-                        await UserModel.findByIdAndUpdate(id, {
+                        await UserModal.findByIdAndUpdate(id, {
                             $set: { password: newHashpassword }
 
                         })
-                       
+
                         res.status(201)
-                        .json({ status: "Success", message: "Password Change successfully" });
+                            .json({ status: "Success", message: "Password Change successfully" });
                     }
                 }
 
             }
             else {
                 res.status(400)
-                .json({ status: "Failed", message: "All field are required" });
+                    .json({ status: "Failed", message: "All field are required" });
 
             }
 
@@ -183,7 +186,6 @@ class UserController {
             console.log('error')
         }
     }
-
     static profile_update = async (req, res) => {
         try {
             //console.log(req.files.image)
@@ -212,40 +214,13 @@ class UserController {
 
                 }
             }
-            const update_profile = await UserModel.findByIdAndUpdate(req.user.id, data)
+            const update_profile = await UserModal.findByIdAndUpdate(req.user.id, data)
             res.status(201)
-            .json({ status: "Success", message: "Profile Update successfully" });
+                .json({ status: "Success", message: "Profile Update successfully" });
         } catch (error) {
             console.log(error)
         }
     }
-
-    static sendEmail = async (name, email) => {
-        // console.log("email sending")
-        //consollog("propertyName")
-        // console.log(email)
-
-        //connenct with the smtp server
-
-        let transporter = await nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-
-            auth: {
-                user: "sujaljha007@gmail.com",
-                pass: "uqltlwovtuaovloc",
-            },
-        });
-        let info = await transporter.sendMail({
-            from: "test@gmail.com", // sender address
-            to: email, // list of receivers
-            subject: "Create course Registration Succesfully", // Subject line
-            text: "hello", // plain text body
-            html: `<b>${name}</b> Registration is successful! please login.. `, // html body
-        });
-        //console.log("Messge sent: %s", info.messageId);
-    };
-
 }
 
 module.exports = UserController;
